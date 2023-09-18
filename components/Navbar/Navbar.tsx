2460fd175363cfe49c4719670da68bd2b66f12c7
Jagrath Shetty <jagrathshettylxix@gmail.com>
2023-09-18 10:15:57 +0530

    tailwind prettier classes sorted

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -53,14 +53,14 @@ function Navbar({}: Props) {
   return (
     <>
       <div
-        className={`fixed z-50 flex items-center justify-between w-full h-20 md:h-24 px-5  lg:px-20 xl:px-72  ${
+        className={`fixed z-50 flex h-20 w-full items-center justify-between px-5 md:h-24  lg:px-20 xl:px-72  ${
           openedDrawer ? "bg-sky-900" : "bg-white lg:bg-transparent"
         }
         }`}
       >
         <Link href="/">
           <h2
-            className={`text-lg md:text-2xl font-semibold ${
+            className={`text-lg font-semibold md:text-2xl ${
               openedDrawer ? "text-white" : "text-black"
             }`}
           >
@@ -73,7 +73,7 @@ function Navbar({}: Props) {
           <ul className="flex justify-around space-x-10 font-medium">
             {navLinks.map((navLink) => (
               <Link key={navLink.index} href={`${navLink.link}`}>
-                <li className="transition duration-300 hover:text-sky-700 2xl:text-lg hover:scale-105">
+                <li className="transition duration-300 hover:scale-105 hover:text-sky-700 2xl:text-lg">
                   {navLink.name}
                 </li>
               </Link>
@@ -82,18 +82,18 @@ function Navbar({}: Props) {
         </div>
 
         {/* Button and Number on LG+ */}
-        <div className="items-center hidden space-x-4 lg:flex">
-          <p className="px-4 py-3 font-semibold transition duration-300 border-2 border-transparent cursor-pointer hover:border-black hover:bg-white">
+        <div className="hidden items-center space-x-4 lg:flex">
+          <p className="cursor-pointer border-2 border-transparent px-4 py-3 font-semibold transition duration-300 hover:border-black hover:bg-white">
             +000123232
           </p>
-          <button className="w-32 h-12 font-semibold text-black transition duration-300 border-2 border-black hover:bg-black hover:text-white active:scale-90">
+          <button className="h-12 w-32 border-2 border-black font-semibold text-black transition duration-300 hover:bg-black hover:text-white active:scale-90">
             Book Now
           </button>
         </div>
 
         <div className="btn lg:hidden">
           <button
-            className={`h-12 md:h-14 px-2 transition duration-300 border-2  rounded-md md:w-32 w-28 active:scale-90 ${
+            className={`h-12 w-28 rounded-md border-2 px-2 transition  duration-300 active:scale-90 md:h-14 md:w-32 ${
               openedDrawer
                 ? "border-white text-white"
                 : "border-sky-950 text-sky-950"
@@ -106,7 +106,7 @@ function Navbar({}: Props) {
           </button>
         </div>
         <div
-          className="w-10 h-10 md:w-14 md:h-14 lg:hidden"
+          className="h-10 w-10 md:h-14 md:w-14 lg:hidden"
           onClick={handleClick}
         >
           <HamburgerIcon openedDrawer={openedDrawer} />
@@ -121,7 +121,7 @@ function Navbar({}: Props) {
             animate={{ y: 0 }}
             exit={{ y: -2000 }}
             transition={{ duration: 0.4 }}
-            className="fixed top-0 z-30 flex flex-row items-start justify-center w-full h-screen pt-32 md:pt-40 bg-sky-900"
+            className="fixed top-0 z-30 flex h-screen w-full flex-row items-start justify-center bg-sky-900 pt-32 md:pt-40"
           >
             <ul className="space-y-10 text-center text-white">
               {navLinks.map((navLink) => (
