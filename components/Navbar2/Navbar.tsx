0746aa320d71ffb15c3df0e29c03c347673c15e9
Jagrath Shetty <jagrathshettylxix@gmail.com>
2023-09-19 19:08:42 +0530

    removed comments from nav

--- components/Navbar2/Navbar.tsx
+++ components/Navbar2/Navbar.tsx
@@ -9,17 +9,6 @@ type Props = {};
 
 function Navbar({}: Props) {
   const [isOpen, setIsOpen] = useState(false);
-  // const [navbar, setNavbar] = useState(false);
-
-  // const { scrollY } = useScroll();
-
-  // useMotionValueEvent(scrollY, "change", (latest) => {
-  //   if (latest > 799) {
-  //     setNavbar(true);
-  //   } else if (latest < 800) {
-  //     setNavbar(false);
-  //   }
-  // });
 
   const genericHamburgerLine = `h-1 w-6 my-1 rounded-full transition ease transform duration-300 bg-white `;
 
@@ -51,25 +40,6 @@ function Navbar({}: Props) {
             Services
           </Link>
           <a href="/#contact" className="text-xl">
-            {/* <button className="cssbuttons-io">
-              <span>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  strokeWidth="2"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  className="lucide lucide-phone"
-                >
-                  <path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z" />
-                </svg>
-                Contact Us
-              </span>
-            </button> */}
             <button className="flex h-14 w-full items-center justify-center rounded border-2 border-white p-4 transition duration-300 hover:scale-105 hover:bg-black/70 active:scale-95">
               Contact Us
             </button>
