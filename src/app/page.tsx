7ca7e723b1bb4a3ee252d079106f3aff072e39db
Jagrath Shetty <jagrathshettylxix@gmail.com>
2023-09-20 00:22:41 +0530

    added react lenis

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,17 @@
+"use client";
 import FaqSection from "../../components/FAQSection/FaqSection";
 import HeroSection2 from "../../components/HeroSection/HeroSection2";
 import WhyUsSection from "../../components/Why Us Section/WhyUsSection";
+import { ReactLenis } from "@studio-freight/react-lenis";
 
 export default function Home() {
   return (
     <main className="w-full">
-      <HeroSection2 />
-      <WhyUsSection />
-      <FaqSection />
+      <ReactLenis root>
+        <HeroSection2 />
+        <WhyUsSection />
+        <FaqSection />
+      </ReactLenis>
     </main>
   );
 }
