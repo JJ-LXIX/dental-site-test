609f3f3e2a58ca4c1ee716bb323571aae3f61e43
Jagrath Shetty <jagrathshettylxix@gmail.com>
2023-09-19 19:07:45 +0530

    added hero, why us , faq sections to page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
-import HeroSection from "../../components/HeroSection/HeroSection";
+import FaqSection from "../../components/FAQSection/FaqSection";
+import HeroSection2 from "../../components/HeroSection/HeroSection2";
+import WhyUsSection from "../../components/Why Us Section/WhyUsSection";
 
 export default function Home() {
   return (
-    <main className="w-full ">
-      <HeroSection />
+    <main className="w-full">
+      <HeroSection2 />
+      <WhyUsSection />
+      <FaqSection />
     </main>
   );
 }
