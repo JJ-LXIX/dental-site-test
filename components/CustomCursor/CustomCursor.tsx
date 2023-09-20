2910d006debe0581ce88a85d0755ca5d21674027
Jagrath Shetty <jagrathshettylxix@gmail.com>
2023-09-20 18:03:14 +0530

    created custom cursor

---
+++ components/CustomCursor/CustomCursor.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { AnimatePresence, motion } from "framer-motion";
+
+type Props = {
+  mousePosition: { x: number; y: number };
+  clickable?: boolean;
+};
+
+export default function CustomCursor({ mousePosition, clickable }: Props) {
+  const variants1 = {
+    default: {
+      x: mousePosition.x - 5,
+      y: mousePosition.y - 5,
+    },
+  };
+  const variants2 = {
+    default: {
+      x: mousePosition.x - 25,
+      y: mousePosition.y - 25,
+    },
+  };
+
+  return (
+    <>
+      <motion.div
+        variants={variants1}
+        animate="default"
+        transition={{ duration: 0 }}
+        className="pointer-events-none fixed  top-0 z-50 h-3 w-3 overflow-hidden rounded-full bg-white mix-blend-difference"
+      ></motion.div>
+      <AnimatePresence>
+        <motion.div
+          variants={variants2}
+          animate="default"
+          transition={{ duration: 0.1 }}
+          className={`pointer-events-none fixed  top-0  z-50  
+        h-12 w-12 overflow-hidden rounded-full mix-blend-difference transition-colors duration-300 ${
+          clickable ? "bg-yellow-300" : "bg-white"
+        }`}
+        ></motion.div>
+      </AnimatePresence>
+    </>
+  );
+}
