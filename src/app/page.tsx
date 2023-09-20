929e707bc256428f6327d2f0eac606b70449bdf5
Jagrath Shetty <jagrathshettylxix@gmail.com>
2023-09-20 14:52:44 +0530

    added isMobile&mousePosition states ,sent as props

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { useEffect, useState } from "react";
 import ContactFormSection from "../../components/ContactFormSection/ContactFormSection";
 import FaqSection from "../../components/FAQSection/FaqSection";
 import HeroSection2 from "../../components/HeroSection/HeroSection2";
@@ -7,13 +8,38 @@ import WhyUsSection from "../../components/Why Us Section/WhyUsSection";
 import { ReactLenis } from "@studio-freight/react-lenis";
 
 export default function Home() {
+  const [isMobile, setIsMobile] = useState(false);
+  const [mousePosition, setMousePosition] = useState({
+    x: 0,
+    y: 0,
+  });
+
+  useEffect(() => {
+    if (window.innerWidth < 1024) {
+      setIsMobile(true);
+    }
+  }, []);
+
+  useEffect(() => {
+    const updateMousePosition = (e: any) => {
+      setMousePosition({
+        x: e.clientX,
+        y: e.clientY,
+      });
+    };
+    window.addEventListener("mousemove", updateMousePosition);
+    return () => {
+      window.removeEventListener("mousemove", updateMousePosition);
+    };
+  }, []);
+
   return (
     <main className="w-full">
       <ReactLenis root>
-        <HeroSection2 />
-        <WhyUsSection />
-        <ServiceSection />
-        <FaqSection />
+        <HeroSection2 isMobile={isMobile} />
+        <WhyUsSection isMobile={isMobile} />
+        <ServiceSection isMobile={isMobile} mousePosition={mousePosition} />
+        <FaqSection isMobile={isMobile} />
         <ContactFormSection />
       </ReactLenis>
     </main>
