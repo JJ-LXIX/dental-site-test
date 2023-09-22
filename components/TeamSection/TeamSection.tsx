278d94660c10b4485c777d5e664d1c52afcb0d48
Jagrath Shetty <jagrathshettylxix@gmail.com>
2023-09-22 11:14:45 +0530

    added dentist images to teamSection

--- components/TeamSection/TeamSection.tsx
+++ components/TeamSection/TeamSection.tsx
@@ -1,6 +1,38 @@
+import Image from "next/image";
 import React from "react";
+import Dentist1 from "../../public/images/team/pexels-linkedin-sales-navigator-2182970.jpg";
+import Dentist2 from "../../public/images/team/pexels-cottonbro-studio-5491144.jpg";
+import Dentist3 from "../../public/images/team/pexels-emmy-e-2381069.jpg";
+import Dentist4 from "../../public/images/team/pexels-andrea-piacquadio-3760263.jpg";
 
 type Props = {};
+
+const dentists = [
+  {
+    image: Dentist1,
+    name: "Dr. Michael Smith, DDS",
+    job: "Senior Dentist",
+    objectPosition: "100% 0",
+  },
+  {
+    image: Dentist2,
+    name: "Dr. Sarah Johnson, DMD",
+    job: "Senior Dentist",
+    objectPosition: "0% 30%",
+  },
+  {
+    image: Dentist3,
+    name: "Dr. Lisa Martinez, DDS",
+    job: "Pediatric Dentist",
+    objectPosition: "50% 50%",
+  },
+  {
+    image: Dentist4,
+    name: "Dr. David Wilson, DMD",
+    job: "Oral Surgeon",
+    objectPosition: "50% 50%",
+  },
+];
 
 export default function TeamSection({}: Props) {
   return (
@@ -11,22 +43,31 @@ export default function TeamSection({}: Props) {
         </h3>
 
         <div className="Team__Images flex flex-col items-center gap-5  pt-10 md:mx-8 md:grid md:grid-cols-2 md:grid-rows-2  xl:w-full  xl:justify-items-center xl:gap-4 xl:pt-0">
-          <div className="h-[60svh] w-10/12 bg-red-200 md:h-[50svh] md:w-full  xl:h-[70vh]">
-            <div className="h-3/4 w-full"></div>
-            <div className="h-1/4 w-full bg-[#202020]"></div>
-          </div>
-          <div className="h-[60svh] w-10/12 bg-red-200 md:h-[50svh] md:w-full xl:h-[70vh]">
-            <div className="h-3/4 w-full"></div>
-            <div className="h-1/4 w-full bg-[#202020]"></div>
-          </div>
-          <div className="h-[60svh] w-10/12 bg-red-200 md:h-[50svh] md:w-full xl:h-[70vh]">
-            <div className="h-3/4 w-full"></div>
-            <div className="h-1/4 w-full bg-[#202020]"></div>
-          </div>
-          <div className="hidden h-[60svh] w-10/12 bg-red-200 md:block md:h-[50svh] md:w-full xl:h-[70vh]">
-            <div className="h-3/4 w-full"></div>
-            <div className="h-1/4 w-full bg-[#202020]"></div>
-          </div>
+          {dentists.map((dentist) => {
+            return (
+              <div className="h-[60svh] w-10/12  md:h-[50svh] md:w-full  xl:h-[70vh]">
+                <div className="relative h-5/6 w-full">
+                  <Image
+                    src={dentist.image}
+                    alt="Dentist Image"
+                    fill
+                    style={{
+                      objectFit: "cover",
+                      objectPosition: dentist.objectPosition,
+                    }}
+                  />
+                </div>
+                <div className="flex h-1/6 w-full flex-col justify-center bg-[#202020] px-2">
+                  <h3 className="text-lg text-white md:text-2xl">
+                    {dentist.name}
+                  </h3>
+                  <h4 className="text-base text-zinc-300 md:text-xl">
+                    {dentist.job}
+                  </h4>
+                </div>
+              </div>
+            );
+          })}
         </div>
       </div>
     </div>
