e4b2217b24273a0fe10c17992df663962db39657
Jagrath Shetty <jagrathshettylxix@gmail.com>
2023-09-25 15:17:21 +0530

    updated column cards

--- components/TestimonialSection/Column.tsx
+++ components/TestimonialSection/Column.tsx
@@ -1,13 +1,17 @@
 import Image from "next/image";
 import React from "react";
 import { MotionValue, motion } from "framer-motion";
+import { Quote } from "lucide-react";
 
-type Props = { images: number[]; y?: MotionValue<number> };
+type Props = {
+  reviews: { name: string; review: string }[];
+  y?: MotionValue<number>;
+};
 
-export default function Column({ images, y }: Props) {
+export default function Column({ reviews, y }: Props) {
   return (
     <motion.div
-      className="relative flex h-full w-1/4 min-w-[15rem] flex-col gap-[2vw] 
+      className="relative flex h-full w-1/4 min-w-[14rem] flex-col gap-[1vw] xl:gap-[2vw] 
       [&:nth-child(1)]:top-[-45%]
       [&:nth-child(2)]:top-[-95%]
       [&:nth-child(3)]:top-[-45%]
@@ -15,18 +19,27 @@ export default function Column({ images, y }: Props) {
       "
       style={{ y }}
     >
-      {images.map((e, index) => {
+      {reviews.map((review, index) => {
         return (
           <div
             key={index}
             className="relative h-full w-full overflow-hidden rounded-lg bg-white"
           >
-            <Image
-              src="/images/Healthcare_worker.png"
-              fill
-              alt="image"
-              style={{ objectFit: "cover" }}
-            />
+            <div className="flex h-full w-full flex-col">
+              {/* Review Text */}
+              <div className="flex h-4/6 w-full items-center p-2 text-center text-2xl xl:px-4 xl:text-3xl">
+                "{review.review}"
+              </div>
+              {/* Image and Name */}
+              <div className="flex h-3/6 w-full flex-col items-center justify-center space-y-4 bg-zinc-100 pt-8">
+                <div className="flex h-full w-full items-center justify-center">
+                  <div className="h-24 w-24 rounded-full bg-red-500"></div>
+                </div>
+                <div className="flex h-full w-full justify-center  text-2xl font-bold ">
+                  {review.name}
+                </div>
+              </div>
+            </div>
           </div>
         );
       })}
